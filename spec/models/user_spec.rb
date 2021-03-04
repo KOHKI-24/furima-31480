72a985fd7cf2fcfa3e6309066205ab7e8475b74e
KOHKI-24 <kohki1124@gmail.com>
2021-03-04 20:00:10 +0900

    テストコード作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it 'emailが空では登録できない' do
+      user = User.new(nickname: 'test', email: '', password: '000000', password_confirmation: '000000')
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
+    end
+  end
 end
+
+
+
+
+# ["Email can't be blank", "Family name can't be blank", "First name kana can't be blank", "Family name kana can't be blank", "Birth day can't be blank"]
