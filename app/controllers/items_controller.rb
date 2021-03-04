7f8aff586efad4462d0d7fb0b4d8972089438b80
KOHKI-24 <kohki1124@gmail.com>
2021-03-04 21:37:01 +0900

    ログイン、ログアウトボタンのルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
   end
+
+  # def destroy
+  #   redirect_to root_path
+  # end
+
+  # def move_to_index
+  #   unless user_signed_in?
+  #     redirect_to action: :index
+  #   end
+  # end
+
 end
