e6389843e51095a3f6931cee1a20303f647d1215
KOHKI-24 <kohki1124@gmail.com>
2021-03-06 16:57:18 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :family_name_kana, presence: true
-  validates :birth_day, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :family_name
+  end
+
+
+  with_options presence: true, format: { with: /\A[ｧ-ﾝﾞﾟ]+\z/, message: '全角カタカナを使用してください' } do
+    validates :first_name_kana
+    validates :family_name_kana
+  end
+
+  with_options presence: true do
+    validates :nickname
+    validates :birth_day
+  end
 end
+
+
